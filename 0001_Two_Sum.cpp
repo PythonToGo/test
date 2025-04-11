04569f186d9d1b6cf9e4f3f8cf45157a34cb6878
Teay <pythontogoplease@gmail.com>
2025-04-11 23:29:18 +0200

    Update LeetCode solution: 0001_Two_Sum.cpp

--- 0001_Two_Sum.cpp
+++ 0001_Two_Sum.cpp
@@ -4,8 +4,7 @@ class Solution {
     unordered_map<int, int> numToIndex;
 
     for (int i = 0; i < nums.size(); ++i) {
-      if (const auto it = numToIndex.find(target - nums
-[i]);
+      if (const auto it = numToIndex.find(target - nums[i]);
           it != numToIndex.cend())
         return {it->second, i};
       numToIndex[nums[i]] = i;
