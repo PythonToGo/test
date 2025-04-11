135be680553b0d8fd3f710e437cb2946deabb43a
Teay <pythontogoplease@gmail.com>
2025-04-11 22:13:46 +0200

    Update LeetCode solution: 0001_Two_Sum.java

--- 0001_Two_Sum.java
+++ 0001_Two_Sum.java
@@ -4,8 +4,8 @@ class Solution {
 
     for (int i = 0; i < nums.length; ++i) {
       if (numToIndex.containsKey(target - nums[i]))
-        return new int[] {numToIndex.get(target - nums
-[i]), i};
+        return new int[] {numToIndex.get(target - nums[i]), 
+i};
       numToIndex.put(nums[i], i);
     }
 
